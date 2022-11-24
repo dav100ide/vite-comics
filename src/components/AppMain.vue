109691caf1135268ff927d65bf2f8947f925c226
dav100ide <oreglio.dev@gmail.com>
2022-11-24 16:00:36 +0100

    aggiunge main

---
+++ src/components/AppMain.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+   name: 'AppMain',
+};
+</script>
+
+<template>
+   <main>
+      <div class="container">
+         <h1>--> content goes here &lt--</h1>
+      </div>
+   </main>
+</template>
+
+<style lang="scss" scoped>
+main {
+   background-color: var(--neutral-color-600);
+   color: var(--neutral-color-100);
+   padding-block: 2rem;
+}
+</style>
